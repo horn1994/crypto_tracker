a273750f86e6493fe092bfcb8d37bf3bcbc5103e
Péter Horn <42172218+horn1994@users.noreply.github.com>
2021-09-17 18:48:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,12 @@ subject = "Sending with SendGrid is Fun"
 content = Content("text/plain", "and easy to do anywhere, even with Python")
 mail = Mail(from_email, to_email, subject, content)
 
+# Get a JSON-ready representation of the Mail object
+mail_json = mail.get()
+
+# Send an HTTP POST request to /mail/send
+response = sg.client.mail.send.post(request_body=mail_json)
+
 while True:
     if datetime.now().time().hour != 0:
         av_cryptos = []
